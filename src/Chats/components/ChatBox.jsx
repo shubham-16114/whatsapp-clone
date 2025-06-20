eb57cee7c3a3c511d7cf53f7aa1d2dbb4a332775
shubham <shubham@shubhams-Mac-mini.local>
2025-06-20 22:01:53 +0530

    feat: index is now shorthand to i

--- src/Chats/components/ChatBox.jsx
+++ src/Chats/components/ChatBox.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const ChatBox = ({ index, name, lastMessage  = 'Last message preview...'
+const ChatBox = ({ i, name, lastMessage  = 'Last message preview...'
 }) => 
    (
    <div
-          key={index}
+          key={i}
           className="flex items-center px-4 py-3 space-x-4 hover:bg-gray-800"
         >
           <div className="flex items-center justify-center w-12 h-12 rounded-full bg-green-500 text-gray-900 font-bold">
